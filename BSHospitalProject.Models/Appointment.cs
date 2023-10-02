85cd66990d58bd13d31a5a1e19e9911a4a7aa01f
Begüm Yünkül <yunkulbegum@gmail.com>
2023-10-03 00:39:04 +0300

    Models ve Data katmanı eklendi. Modeller oluşturuldu.

---
+++ BSHospitalProject.Models/Appointment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSHospitalProject.Models
+{
+    public class Appointment : BaseModel
+    {
+
+    }
+}
---
+++ BSHospitalProject.Models/Branch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSHospitalProject.Models
+{
+    public class Branch:BaseModel
+    {
+        public string BranchName { get; set; }
+        public string DepartmentName { get; set; }
+        public string DoctorName { get; set; }
+        public string InternalPhoneNum { get; set; }
+        public int HospitalNo { get; set; }
+    }
+}

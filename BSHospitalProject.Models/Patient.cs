9ddd4d3a88ecebbf96a7cdd55f8a1a31bd57cc43
Begüm Yünkül <yunkulbegum@gmail.com>
2023-10-09 00:40:59 +0300

    migration eklendi ve tablolar eklendi sql'e

--- BSHospitalProject.Models/Patient.cs
+++ BSHospitalProject.Models/Patient.cs
@@ -14,11 +14,21 @@ namespace BSHospitalProject.Models
         public int Age { get; set; }
         public bool IsSick { get; set; } = false;
         public bool HaveSurgery { get; set; } = false;
+<<<<<<< HEAD
         public bool IsAllergic { get; set; } = false;
         public int? DoctorId { get; set; }
         public int? HospitalId { get; set; }
         public int? DepartmentId { get; set; }
         
+=======
+        public bool IsAllergic { get; set; }=false;
+        public bool TakeMedicine { get; set; }=false;
+        public bool HaveAppointment { get; set; }=false;
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public int HospitalId { get; set; }
+        public int DepartmentId { get; set; }
+>>>>>>> 8fb093aba1f4680ddc32ad4dd51e4a8e203611e5
 
         public virtual Doctor Doctor { get; set; }
         public virtual Hospital Hospital { get; set; }

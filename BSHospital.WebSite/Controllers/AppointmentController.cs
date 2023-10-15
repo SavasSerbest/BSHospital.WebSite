b85652274eba55d4d0146ab00a45e90d347d42f9
Begüm Yünkül <yunkulbegum@gmail.com>
2023-10-15 19:28:28 +0300

    Controller'a yeni controller, yeni model ve  agentController'in View sayfası eklendi.

--- BSHospital.WebSite/Controllers/AppointmentController.cs
+++ BSHospital.WebSite/Controllers/AppointmentController.cs
@@ -1,4 +1,5 @@
 ﻿using BSHospital.Repository.Shared.Abstract;
+using BSHospitalProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BSHospital.WebSite.Controllers
@@ -12,6 +13,35 @@ namespace BSHospital.WebSite.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Appointment appointment)
+        {
+            unitOfWork.Appointments.Add(appointment);
+            unitOfWork.Save();
+            return View();
+        }
+
+        public IActionResult GetAll() 
+        {
+            return Json(new {data= unitOfWork.Appointments.GetAll().ToList()});
+        }
+
+        [HttpPost]
+        public IActionResult DeleteById(int id) 
+        {
+            unitOfWork.Appointments.DeleteById(id);
+            unitOfWork.Save();
+            return Ok("Başarıyla silindi");
+        }
+
+        [HttpPost]
+        public IActionResult Update(Appointment appointment)
+        {
+            unitOfWork.Appointments.Update(appointment);
+            unitOfWork.Save();
+            return Ok();
         }
     }
 }
